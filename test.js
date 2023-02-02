9374ca498ed6ddf956ebc4d3927df5696fbdfa87
Mikhailo Garmotko <mgarmotko@gmail.com>
2023-02-03 01:20:07 +0200

    console

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
-// const str = `The Tao gave birth to machine language.  Machine language gave birth"  + "\n" +
-// "to the assembler." + "\n" +
-// "The assembler gave birth to the compiler.  Now there are ten thousand" + "\n" +
-// "languages."+ "\n" +
-// "Each language has its purpose, however humble.  Each language"+ "\n" +
-// "expresses the Yin and Yang of software.  Each language has its place within"+ "\n" +
-// "the Tao."+ "\n" +
-// "But do not program in COBOL if you can avoid it."+ "\n" +
-// "        -- Geoffrey James,"The Tao of Programming"`;
+const str1 = `The Tao gave birth to machine language.  Machine language gave birth"  + "\n" +
+"to the assembler." + "\n" +
+"The assembler gave birth to the compiler.  Now there are ten thousand" + "\n" +
+"languages."+ "\n" +
+"Each language has its purpose, however humble.  Each language"+ "\n" +
+"expresses the Yin and Yang of software.  Each language has its place within"+ "\n" +
+"the Tao."+ "\n" +
+"But do not program in COBOL if you can avoid it."+ "\n" +
+"        -- Geoffrey James,"The Tao of Programming"`;
 
-const str = `C makes it easy for you to shoot yourself in the foot. C++ makes that harder, but when you do, it blows away your whole leg. (с) Bjarne Stroustrup`;
+const str2 = `C makes it easy for you to shoot yourself in the foot. C++ makes that harder, but when you do, it blows away your whole leg. (с) Bjarne Stroustrup`;
 
 const findFirstNonRepeatedChar = (str) => {
   let cash = "";
@@ -38,4 +38,5 @@ const splitArray = (str) => {
 
   return findFirstNonRepeatedChar(result.join());
 };
-console.log(splitArray(str));
+console.log(splitArray(str1));
+console.log(splitArray(str2));
